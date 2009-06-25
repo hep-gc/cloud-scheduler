ef1633d70f617520db8036d9e425a2837259e92e
Duncan Penfold-Brown <dpb@heplw16.phys.UVic.CA>
2009-06-24 18:21:48 -0700

    Change in cluster storage structure.
    Moved cluster storage class from cloud_scheduler, and merged cluster storage information with the "CloudManager" class from the previous cloud_management. cloud_management now contains a general "Cluster" class with all general instance variables and methods (stubs for workspace commands), and will contain subclasses that implement specific cloudware functionality. Did this to re-vamp the ability to support multiple cloudwares (Nimbus, OpenNebula) in an OO way.

--- cloud-scheduler/cloud_management.py
+++ cloud-scheduler/cloud_management.py
@@ -3,20 +3,65 @@
 ## Auth: Duncan Penfold-Brown. 6/15/2009.
 
 ##
-## The CloudManager superclass serves as something of an interface for cloud
-## management functionality. Each of its subclasses should should correspond to
-## a specific implementation for cloud management functionality. That is, each
-## subclass should implement the functions in CloudManager according to a specific
-## software. Currently, only a Nimbus subclass exists. If support is desired for
-## other cloud solutions, an other subclasses (such as an OpenNebula subclass) might 
+## The Cluster superclass provides the basic structure for cluster information,
+## and provides the framework (interface) for cloud management functionality.
+## Each of its subclasses should should correspond to a specific implementation 
+## for cloud management functionality. That is, each subclass should implement 
+## the functions in the Cluster superclass according to a specific software. 
+## Currently, only a Nimbus subclass exists. If support is desired for other 
+## cloud solutions, other subclasses (such as an OpenNebula subclass) might 
 ## also be desired.
 ##
-## To import specific subclasses, simply alter the main scheduler's import lines.
-##     e.g.:    from cloud_management import  NimbusCloud
+## To import specific subclasses only, simply alter the cloud_scheduler import lines.
+##     e.g.:    from cloud_management import  NimbusCluster
 ##
 
-class CloudManager:
-    
+class Cluster:
+   
+    # Instance variables (preset to defaults)
+    name            = 'def_cluster'
+    network_address = '0.0.0.0'
+    vm_slots        = 0
+    cpu_cores       = 0
+    storageGB       = 0
+    memoryMB        = 0
+    x86             = 'no'
+    x86_64          = 'no'
+    network_public  = 'no'
+    network_private = 'no'
+
+    # Instance methods
+
+    # Constructor
+    def __init__(self, ):
+        print "dbg - New Cluster created"
+
+    # Set Cluster attributes from a parameter list
+    def populate(self, attr_list):
+        (self.name, self.network_address, self.vm_slots, self.cpu_cores, \
+          self.storageGB, self.memoryMB, self.x86, self.x86_64, \
+          self.network_public, self.network_private) = attr_list;
+        
+        self.network_private = self.network_private.rstrip("\n");
+        print "dbg - Cluster populated successfully"
+        
+    # Print cluster information
+    def print_cluster(self):
+        print "-" * 80
+        print "Name:\t\t%s" % (self.name)
+        print "Address:\t%s" % (self.network_address)
+        print "VM Slots:\t%s" % (self.vm_slots)
+        print "CPU Cores:\t%s" % (self.cpu_cores)
+        print "Storage:\t%s" % (self.storageGB)
+        print "Memory:\t\t%s" % (self.memoryMB)
+        print "x86:\t\t%s" % (self.x86)
+        print "x86_64:\t\t%s" % (self.x86_64)
+        print "Network Pub:\t%s" % (self.network_public)
+        print "Network Priv:\t%s" % (self.network_private)
+        print "-" * 80
+        
+
+    # Workspace manipulation methods 
     def workspace_create(self):
         print 'This method should be defined by all subclasses of CloudManager\n'
         assert 0, 'Must define workspace_create'
@@ -32,7 +77,16 @@ class CloudManager:
 ## Implements cloud management functionality with the Nimbus service as part of
 ## the Globus Toolkit.
 
-class NimbusCloud(CloudManager):
+class NimbusCluster(Cluster):
+
+    # NimbusCluster specific instance variables
+
+    # NimbusCluster specific instance methods
+    
+    # Overridden constructor
+    def __init__(self, ):
+        print "dbg - New NimbusCluster created"
+
 
     def workspace_create(self):
         print 'dbg - Nimbus cloud create command'
--- cloud-scheduler/cloud_scheduler.py
+++ cloud-scheduler/cloud_scheduler.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python
 
-## GIT test for commit on new branch
-
 ## Auth: Duncan Penfold-Brown. 6/15/2009
 
 ##
@@ -24,51 +22,8 @@ import cloud_management
 ## Class Definitions
 ##
 
-class Cluster:
-    
-    # Instance variables (preset to defaults)
-    name            = 'def_cluster'
-    network_address = '0.0.0.0'
-    vm_slots        = 0
-    cpu_cores       = 0
-    storageGB       = 0
-    memoryMB        = 0
-    x86             = 'no'
-    x86_64          = 'no'
-    network_public  = 'no'
-    network_private = 'no'
 
-    # Instance methods
-
-    # Constructor
-    def __init__(self, ):
-        print "dbg - New Cluster created"
-
-    # Set Cluster attributes from a parameter list
-    def populate(self, attr_list):
-        (self.name, self.network_address, self.vm_slots, self.cpu_cores, \
-          self.storageGB, self.memoryMB, self.x86, self.x86_64, \
-          self.network_public, self.network_private) = attr_list;
-        
-        self.network_private = self.network_private.rstrip("\n");
-        print "dbg - Cluster populated successfully"
-        
-    # Print cluster information
-    def print_cluster(self):
-        print "-" * 80
-        print "Name:\t\t%s" % (self.name)
-        print "Address:\t%s" % (self.network_address)
-        print "VM Slots:\t%s" % (self.vm_slots)
-        print "CPU Cores:\t%s" % (self.cpu_cores)
-        print "Storage:\t%s" % (self.storageGB)
-        print "Memory:\t\t%s" % (self.memoryMB)
-        print "x86:\t\t%s" % (self.x86)
-        print "x86_64:\t\t%s" % (self.x86_64)
-        print "Network Pub:\t%s" % (self.network_public)
-        print "Network Priv:\t%s" % (self.network_private)
-        print "-" * 80
-        
-
+# Move to cloud_management?
 class ResourcePool:
     
     # Instance variables    
@@ -84,6 +39,8 @@ class ResourcePool:
     def add_resource(self, cluster):
         self.resources.append(cluster)
 
+    # TODO: A print resource pool function that will print the name+address of every
+    #       cluster in the resource pool
 
 ##
 ## Functions
@@ -98,9 +55,6 @@ def main(argv = sys.argv):
         sys.exit(1)
     
     cloud_conffile = argv[1]
-
-    # Create a cloud manager
-    testCloud = cloud_management.NimbusCloud()
 
     # Create a resource pool
     cloud_resources = ResourcePool()
@@ -130,7 +84,9 @@ def readCloudConfig(config_file, rsrc_pool):
         cluster_attr = line.split('/') 
         print "dbg - %s" % (cluster_attr)
 
-        new_cluster = Cluster()
+        # Currently working with only general clusters. Support for checking
+        # which type of cluster to create coming in future (add as a config file parameter: Nimbus, OpenNebula...)
+        new_cluster = cloud_management.Cluster()
         new_cluster.populate(cluster_attr)
         new_cluster.print_cluster()
         
