31e4b246e0f73bf87b359db2c2199aea51364265
Duncan Penfold-Brown <dpb@heplw16.phys.UVic.CA>
2009-07-22 15:18:38 -0700

    Created a very basic script to clean the cloud scheduler directory of log files, tmp files, and python link files.

---
+++ cloud-scheduler/cs_clean.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# A simple script to clean log, link, and temp files created by the cloud
+# scheduler.
+# Usage: ./cs_clean.sh [-i : confirm all file deletions]
+
+rm $1 *.log
+rm $1 *.pyc
+rm $1 tmp_*
+
