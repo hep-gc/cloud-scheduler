e5dfd9afe85a8e6ee994a6c23a7f5b7e77e29a48
Ian Gable <igable@uvic.ca>
2009-10-21 14:30:19 -0700

    After discussions with Patrick we have moved to a dual GPLv3 or Apache v2 License.

--- cloud_management.py
+++ cloud_management.py
@@ -1,21 +1,9 @@
 #!/usr/bin/env python
 # vim: set expandtab ts=4 sw=4:
 
-# Copyright 2009 University of Victoria
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-# http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-#
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
+# Copyright (C) 2009 University of Victoria
+# You may distribute under the terms of either the GNU General Public
+# License or the Apache v2 License, as specified in the README file.
 
 ## Auth: Duncan Penfold-Brown. 6/15/2009.
 
