f66ff8864a9d3b919baebdb8be0698291cd766e7
Duncan Penfold-Brown <dpb@heplw31.phys.UVic.CA>
2010-01-29 18:40:56 -0800

    Changed ResourcePool method get_resourceBF to support returning the two most balanced clusters as primary and secondary resources.
    Supports a varied retry when VM creation on a single resource fails - we can retry on a different (next best) resource.

--- cloudscheduler/cloud_management.py
+++ cloudscheduler/cloud_management.py
@@ -227,6 +227,9 @@ class ResourcePool:
     # Returns a resource that fits given requirements and fits some balance
     # criteria between clusters (for example, lowest current load or most free
     # resources of the fitting clusters).
+    # Returns the first find as the primary balanced cluster choice, and returns
+    # a secondary fitting cluster if available (otherwise, None is returned in
+    # place of a secondary cluster).
     # Built to support "Cluster-Balanced Fit Scheduling"
     # Note: Currently, we are considering the "most balanced" cluster to be that
     # with the fewest running VMs on it. This is to minimize and balance network
@@ -240,8 +243,12 @@ class ResourcePool:
     #    memory   - the amount of memory (RAM) the VM requires
     #    cpucores  - the number of cores that a VM requires (dedicated? or general?)
     #    storage   - the amount of scratch space the VM requires
-    # Return: returns a Cluster object if one is found that fits VM requirments
-    #         Otherwise, returns the 'None' object
+    # Return: returns a tuple of cluster objects. The first, or primary cluster, is the
+    #         most balanced fit. The second, or secondary, is an alternative fitting
+    #         cluster.
+    #         Normal return, (Primary_Cluster, Secondary_Cluster)
+    #         If no secondary cluster is found, (Cluster, None) is returned.
+    #         If no fitting clusters are found, (None, None) is returned.
     def get_resourceBF(self, network, cpuarch, memory, cpucores, storage):
 
         # Get a list of fitting clusters
@@ -250,17 +257,39 @@ class ResourcePool:
         # If list is empty (no resources fit), return None
         if len(fitting_clusters) == 0:
             log.debug("No clusters fit requirements. Fitting resources list is empty.")
-            return None
+            return (None, None)
+        
+        # If the list has only 1 item, return immediately
+        if len(fitting_clusters) == 1:
+            log.debug("Only one cluster fits parameters. Returning that cluster.")
+            return (fitting_clusters[0], None)
 
-        # Iterate through fitting clusters - save "most balanced" cluster. (LINEAR search)
-        # Note: mb_cluster stands for "most balanced cluster"
-        mb_cluster = fitting_clusters.pop()
-        mb_cluster_vms = mb_cluster.num_vms()
+        # Set the most-balanced and next-most-balanced initial values
+        # Note: mostbal_cluster stands for "most balanced cluster"
+        # Note: nextbal_cluster stands for "next most balanced cluster"
+        cluster1 = fitting_clusters.pop()
+        cluster2 = fitting_clusters.pop()
+        
+        if (cluster1.num_vms() < cluster2.num_vms()):
+            mostbal_cluster = cluster1
+            nextbal_cluster = cluster2
+        else:
+            mostbal_cluster = cluster2
+            nextbal_cluster = cluster1
+        
+        mostbal_vms = mostbal_cluster.num_vms()
+        nextbal_vms = nextbal_cluster.num_vms()
+
+        # Iterate through fitting clusters to check for most and next balanced clusters. (LINEAR search)
         for cluster in fitting_clusters:
             # If considered cluster has fewer running VMs, set it as the most balanced cluster
-            if (cluster.num_vms() < mb_cluster_vms):
-                mb_cluster = cluster
-                mb_cluster_vms = cluster.num_vms()
+            if (cluster.num_vms() < mostbal_vms):
+                mostbal_cluster = cluster
+                mostbal_vms = cluster.num_vms()
+            elif (cluster.num_vms() < nextbal_vms):
+                nextbal_cluster = cluster
+                nextbal_vms = cluster.unm_vms()
 
         # Return the most balanced cluster after considering all fitting clusters.
-        return mb_cluster
+        return (mostbal_cluster, nextbal_cluster)
+
