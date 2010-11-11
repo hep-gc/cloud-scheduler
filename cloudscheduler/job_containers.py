928761d23ad81d2f2001b75169d3d63da7919777
Michael Paterson <mhp@uvic.ca>
2010-11-10 16:09:46 -0800

    Added get unscheduled jobs by type to use in retiring selection

--- cloudscheduler/job_containers.py
+++ cloudscheduler/job_containers.py
@@ -153,6 +153,14 @@ class JobContainer():
     def get_unscheduled_jobs_by_users(self, prioritized=False):
         pass
 
+    # Get a list of all unscheduled jobs per type.
+    # Returns dictionary where the items are:
+    # {type, [list of unscheduled jobs]}
+    # If prioritized i True, then the returned lists of jobs will be sorted by job.priority, high to low.
+    @abstractmethod
+    def get_unscheduled_jobs_by_type(self, prioritized=False):
+        pass
+
     # Get a list of all high priority jobs in the container, or [] if there are no high priority jobs.
     # A job is said to have high priority if job.high_priority != 0
     @abstractmethod
@@ -340,6 +348,19 @@ class HashTableJobContainer(JobContainer):
                     return_value[job.user] = []
                 return_value[job.user].append(job)
             # Now lets sort if needed.
+            if prioritized:
+                for job_list in return_value.values():
+                    job_list.sort(key=lambda job: job.get_priority(), reverse=True)
+            return return_value
+
+    def get_unscheduled_jobs_by_type(self, prioritized=False):
+        with self.lock:
+            return_value = {}
+            for job in self.new_jobs.values():
+                if job.req_vmtype not in return_value:
+                    return_value[job.req_vmtype] = []
+                return_value[job.req_vmtype].append(job)
+            # Now sort if needed.
             if prioritized:
                 for job_list in return_value.values():
                     job_list.sort(key=lambda job: job.get_priority(), reverse=True)
