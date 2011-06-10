adba24538f7b81e68ea75bfeb147bb1c77e1a5fc
Michael Paterson <mhp@uvic.ca>
2011-06-10 12:02:51 -0700

    Altered scheduling behaviour to only attempt a job of each type for a user then complete cycle rather than trying all jobs for a user until successful.

--- cloudscheduler/job_containers.py
+++ cloudscheduler/job_containers.py
@@ -504,3 +504,17 @@ class HashTableJobContainer(JobContainer):
 
             return matching_jobs
 
+    def get_unscheduled_user_jobs_by_type(self, user, prioritized=False):
+        with self.lock:
+            unsched = self.get_unscheduled_jobs_by_users()
+            return_value = {}
+            if user in unsched.keys():
+                for job in unsched[user]:
+                    if job.req_vmtype not in return_value:
+                        return_value[job.req_vmtype] = []
+                    return_value[job.req_vmtype].append(job)
+            # Sort if needed
+            if prioritized:
+                for job_list in return_value.values():
+                    job_list.sort(key=lambda job: job.get_priority(), reverse=True)
+        return return_value
