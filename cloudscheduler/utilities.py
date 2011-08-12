46f73d59cfc8ee05b1491353e6e1266147b949b1
Michael Paterson <mhp@uvic.ca>
2011-08-12 10:08:23 -0700

    added python docstrings to all functions - moved code comments into docstring where they existed

--- cloudscheduler/utilities.py
+++ cloudscheduler/utilities.py
@@ -41,6 +41,7 @@ class NullHandler(logging.Handler):
         pass
 
 def get_cloudscheduler_logger():
+    """Gets a reference to the 'cloudscheduler' log handle."""
     logging.VERBOSE = LEVELS["VERBOSE"]
     logging.addLevelName(logging.VERBOSE, "VERBOSE")
     log = logging.getLogger("cloudscheduler")
@@ -51,15 +52,18 @@ def get_cloudscheduler_logger():
 
 
 def get_hostname_from_url(url):
+    """Return the hostname parsed from a full url."""
     return urlparse(url)[1].split(":")[0]
 
 def get_or_none(config, section, value):
+    """Return the value of a config option if it exists, none otherwise."""
     if config.has_option(section, value):
         return config.get(section, value)
     else:
         return None
 
 def splitnstrip(sep, str):
+    """Return a list of items trimed of excess whitespace from a string(typically comma separated)."""
     return [x.strip() for x in str.split(sep)];
 
 
@@ -94,16 +98,18 @@ def get_globus_path(executable="grid-proxy-init"):
 
 
 
-# This utility function will extract the subject DN from an x509
-# certificate.
-#
-# Note that this method is affected by the use_pyopenssl config variable.
-# If use_pyopenssl is True, then the pyopenssl librairies will be used to extract
-# the certificate subject.  Else a openssl subprocess will be forked to
-# extract the info out of the certificate.
-#
-# It requires the openssl package to be installed.
+
 def get_cert_DN(cert_file_path):
+    """This utility function will extract the subject DN from an x509 certificate.
+
+       Note that this method is affected by the use_pyopenssl config variable.
+       If use_pyopenssl is True, then the pyopenssl librairies will be used to 
+       extract the certificate subject.  Else a openssl subprocess will be 
+       forked to extract the info out of the certificate.
+
+       It requires the openssl package to be installed.
+       
+    """
     if config.use_pyopenssl:
         try:
             cert_file = open(cert_file_path, 'r')
@@ -126,18 +132,21 @@ def get_cert_DN(cert_file_path):
             return None
     
 
-# This utility function will extract the expiry time from an x509
-# certificate.
-# It requires the openssl package to be installed.
-# Returns a datetime instance, with UTC time (naive).
-#
-# Note that this method is affected by the use_pyopenssl config variable.
-# If use_pyopenssl is True, then the pyopenssl librairies will be used to extract
-# the certificate expiry time.  Else a openssl subprocess will be forked to
-# extract the info out of the certificate.
-#
-# Returns None on error
+
 def get_cert_expiry_time(cert_file_path):
+    """This utility function will extract the expiry time from an x509 certificate.
+    
+    It requires the openssl package to be installed.
+    Returns a datetime instance, with UTC time (naive).
+
+    Note that this method is affected by the use_pyopenssl config variable.
+    If use_pyopenssl is True, then the pyopenssl librairies will be used to extract
+    the certificate expiry time.  Else a openssl subprocess will be forked to
+    extract the info out of the certificate.
+
+    Returns None on error
+
+    """
     if config.use_pyopenssl:
         try:
             cert_file = open(cert_file_path, 'r')
@@ -205,26 +214,38 @@ def match_host_with_condor_host(hostname, condor_hostname):
     return False
 
 class CircleQueue():
-    def __init__(self, length):
+    """Represents a Circular queue of specified length."""
+    def __init__(self, length=10):
+        """Initializes the new circular queue.
+        
+        Keywords:
+        length, default 10
+        
+        """
         self.data = [None for x in range(0, length)]
 
     def append(self, x):
+        """Append item to the end of the queue while removing the head."""
         self.data.pop(0)
         self.data.append(x)
 
     def get(self):
+        """Returns the queue as a list."""
         return self.data
 
     def clear(self):
+        """Resets the queue to empty state filled with None values."""
         self.data = [None for x in range(0, len(self.data))]
 
     def min_use(self):
+        """Checks the head of the queue for a None value to determine if is queue full."""
         min_use = True
         if self.data[0] == None:
             min_use = False
         return min_use
 
     def length(self):
+        """Returns the number of non-None values to get the actual length of queue."""
         length = 0
         for x in self.data:
             if x != None:
@@ -232,11 +253,14 @@ class CircleQueue():
         return length
 
 class ErrTrackQueue(CircleQueue):
+    """Error Tracking Queue - Keeps a True/False record of each VM Boot."""
     def __init__(self, name):
+        """Initializes new queue with the configured length."""
         CircleQueue.__init__(self, config.ban_min_track)
         self.name = name
 
     def dist_true(self):
+        """Calculate the distribution of True(succuessful) starts."""
         tc = 0
         for x in self.data:
             if x:
@@ -244,15 +268,19 @@ class ErrTrackQueue(CircleQueue):
         return (float(tc) / float(len(self.data))) if len(self.data) > 0 else 0
 
     def dist_false(self):
+        """Calculate the distribution of False(failed) starts."""
         return 1.0 - self.dist_true()
     
 class JobRunTrackQueue(CircleQueue):
+    """Job Run-[time] Tracking Queue. Keeps a list of job runtimes for  stats purposes."""
     def __init__(self, name):
+        """Initizlizes new queue, of length 10."""
         CircleQueue.__init__(self, 10)
         self.name = name
         self.avg = 0
         
     def average(self):
+        """"Returns the average run-time of jobs in the queue."""
         total = 0
         for x in self.data:
             if x:
@@ -261,10 +289,11 @@ class JobRunTrackQueue(CircleQueue):
             self.avg = total / self.length()
         return self.avg
 
-# Timeout feature for subprocess.Popen - polls the process for timeout seconds waiting for it to complete
-# If the process has exited return False (process did not timeout)
-# Else if the process times out attempt to terminate the process or kill if terminate fails and return True (process timed out)
 def check_popen_timeout(process, timeout=180):
+    """ Timeout feature for subprocess.Popen - polls the process for timeout seconds waiting for it to complete
+        If the process has exited return False (process did not timeout)
+        Else if the process times out attempt to terminate the process or kill if terminate fails and return True (process timed out)
+    """
     log = get_cloudscheduler_logger()
     ret = True
     while timeout > 0:
