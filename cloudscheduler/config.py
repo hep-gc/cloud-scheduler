5ba2c4c7b9a4b0eaaa57151818ea01a2260c13b0
Michael Paterson <mhp@uvic.ca>
2012-08-02 10:25:00 -0700

    Cluster boot timeout function, ignoring a few returns

--- cloudscheduler/config.py
+++ cloudscheduler/config.py
@@ -79,6 +79,7 @@ vm_proxy_refresher_interval = -1 # The current default is not to refresh the VM 
 vm_proxy_renewal_threshold = 60 * 60 # 60 minutes default
 vm_proxy_shutdown_threshold = 30 * 60 # 30 minutes default
 vm_connection_fail_threshold = 30 * 60 # 30 minutes default
+vm_start_running_timeout = -1 # Unlimited time
 vm_idle_threshold = 5 * 60 # 5 minute default
 max_starting_vm = -1
 max_destroy_threads = 10
@@ -178,6 +179,7 @@ def setup(path=None):
     global vm_proxy_renewal_threshold
     global vm_proxy_shutdown_threshold
     global vm_connection_fail_threshold
+    global vm_start_running_timeout
     global vm_idle_threshold
     global max_starting_vm
     global proxy_cache_dir
@@ -610,6 +612,14 @@ def setup(path=None):
             vm_idle_threshold = config_file.getint("global", "vm_idle_threshold")
         except ValueError:
             print "Configuration file problem: vm_idle_threshold must be an " \
+                  "integer value."
+            sys.exit(1)
+
+    if config_file.has_option("global", "vm_start_running_timeout"):
+        try:
+            vm_start_running_timeout = config_file.getint("global", "vm_start_running_timeout")
+        except ValueError:
+            print "Configuration file problem: vm_start_running_timeout must be an " \
                   "integer value."
             sys.exit(1)
 
