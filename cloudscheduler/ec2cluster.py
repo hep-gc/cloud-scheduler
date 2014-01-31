c54fb8507ac03daebfe1a9dc487cc6efeaf45e2c
Michael Paterson <mhp@uvic.ca>
2014-01-31 13:56:39 -0800

    try to append the group configured available security groups when non-specified in jdl #292

--- cloudscheduler/ec2cluster.py
+++ cloudscheduler/ec2cluster.py
@@ -158,8 +158,9 @@ class EC2Cluster(cluster_tools.ICluster):
                 if group in self.security_groups:
                     sec_group.append(group)
             if len(sec_group) == 0:
-                log.warning("No matching security groups - trying default")
-                sec_group.append("default")
+                log.warning("No matching security groups - trying default config")
+                sec_group = self.security_groups
+                #sec_group.append("default") - don't just append default use what is in cloud_resources.conf for this cloud
         else:
             sec_group = self.security_groups
 
