65332c3ce0c2e2e3665f3d99fc10c5d5473ee5f9
Michael Paterson <mhp@uvic.ca>
2014-06-24 12:32:29 -0700

    add more formatting and cloud-config start if missing #294

--- cloudscheduler/cloud_init_util.py
+++ cloudscheduler/cloud_init_util.py
@@ -10,10 +10,13 @@ def inject_customizations(pre_init, cloud_init):
     # cloud init should be a list of file contents
     # Need to see if the write_files preamble exists
     found_write_files = False
+    found_cloud_init = False
     index_of_write_files = 0
     index_pre = 0
     for k, initscript in enumerate(pre_init):
         for i, line in enumerate(initscript): # need to iterate with index counts as well
+            if line == '#cloud-config':
+                found_cloud_init = True
             if line == 'write_files:':
                 found_write_files = True
                 index_of_write_files = i # save the index to insert at later
@@ -28,6 +31,8 @@ def inject_customizations(pre_init, cloud_init):
     else:
         # need to insert at the index
         pre_init[index_pre].insert(index_of_write_files+1, '\n'.join(cloud_init))
+    if not found_cloud_init:
+        pre_init[index_pre] = ''.join(['#cloud-config\n', pre_init[index_pre]])
     return pre_init
     
 def build_write_files_cloud_init(custom_tasks):
@@ -43,7 +48,12 @@ def build_write_files_cloud_init(custom_tasks):
         if not task[1][0] or task[1][0] != '/':
             continue
         cloud_init.append('-   content: |')
-        cloud_init.append(''.join(['        ', task[0]]))
+        formatted_task = []
+        lines = task[0].split('\n')
+        for line in lines:
+            formatted_task.append(''.join(['        ', line]))
+        
+        cloud_init.append('\n'.join(formatted_task))
         cloud_init.append('    path: %s' % task[1])
     return cloud_init
         
