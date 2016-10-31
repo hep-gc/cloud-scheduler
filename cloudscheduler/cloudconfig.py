9a6a99ba926acf912ed8bd94f734a1b5ac3fbcd4
Michael Paterson <mhp@uvic.ca>
2016-10-31 09:06:40 -0700

    add azure's service_name as valid cloud config value

--- cloudscheduler/cloudconfig.py
+++ cloudscheduler/cloudconfig.py
@@ -94,8 +94,8 @@ def verify_sections_base(conf, name):
                           'image_attach_device', 'key_name', 'keycert', 'max_vm_mem', 'max_vm_storage', 'memory',
                           'networks', 'password', 'placement_zone', 'port', 'priority', 'project_id', 'regions',
                           'reverse_dns_lookup', 'scratch_attach_device', 'secret_access_key', 'secret_file',
-                          'secure_connection', 'security_group', 'storage', 'temp_lease_storage', 'tenant_name',
-                          'total_cpu_cores', 'username', 'vm_keep_alive', 'vm_lifetime', 'vm_slots'}
+                          'secure_connection', 'security_group', 'service_name', 'storage', 'temp_lease_storage',
+                          'tenant_name', 'total_cpu_cores', 'username', 'vm_keep_alive', 'vm_lifetime', 'vm_slots'}
     options = set(conf.options(name))
     diff = options - valid_option_names
     if len(diff) > 0:
